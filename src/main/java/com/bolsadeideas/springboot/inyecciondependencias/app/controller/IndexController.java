550aa1957f06e59837128eea6970608aaa568839
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-19 15:03:51 -0600

    Clase Servicio

--- src/main/java/com/bolsadeideas/springboot/inyecciondependencias/app/controller/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/inyecciondependencias/app/controller/IndexController.java
@@ -3,13 +3,19 @@ package com.bolsadeideas.springboot.inyecciondependencias.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+//	Ejemplo pero sin usar Inyeccion de dependecias
+import com.bolsadeideas.springboot.inyecciondependencias.app.models.service.MiServicio;
 
 @Controller
 public class IndexController {
 	
+	//	Instanciando MiServicio como Atributo
+	private MiServicio servicio = new MiServicio();
+	
 	//	Metodos handler de controlador (siempre publicos)
 	@GetMapping({"", "/", "/index"})
 	public String index(Model model) {
+		model.addAttribute("objeto", servicio.operacion());
 		
 		return "index";
 	}
---
+++ src/main/java/com/bolsadeideas/springboot/inyecciondependencias/app/models/service/MiServicio.java
@@ -0,0 +1,10 @@
+package com.bolsadeideas.springboot.inyecciondependencias.app.models.service;
+
+public class MiServicio {
+	
+	public String operacion() {
+		return "Ejecutando algun proceso importante..";
+	}
+	
+	
+}
