401e20c067a9ee771d59434a1772f7e8c8c9350d
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-19 15:16:31 -0600

    Inyeccion de dependencias con @Autowired
    Implementando Inyeccion de dependencias con @Autowired

--- src/main/java/com/bolsadeideas/springboot/inyecciondependencias/app/controller/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/inyecciondependencias/app/controller/IndexController.java
@@ -1,16 +1,19 @@
 package com.bolsadeideas.springboot.inyecciondependencias.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//	Ejemplo pero sin usar Inyeccion de dependecias
 import com.bolsadeideas.springboot.inyecciondependencias.app.models.service.MiServicio;
 
 @Controller
 public class IndexController {
 	
-	//	Instanciando MiServicio como Atributo
-	private MiServicio servicio = new MiServicio();
+	/*	Instanciando MiServicio como Atributo
+	private MiServicio servicio = new MiServicio();*/
+	//	Inyectando el objeto con @Autowired
+	@Autowired
+	private MiServicio servicio;
 	
 	//	Metodos handler de controlador (siempre publicos)
 	@GetMapping({"", "/", "/index"})
--- src/main/java/com/bolsadeideas/springboot/inyecciondependencias/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/inyecciondependencias/app/models/service/MiServicio.java
@@ -1,5 +1,12 @@
+//	Es necesario que esten dentro del mismo paquete, en este caso: (package com.bolsadeideas.springboot.inyecciondependencias.app)
 package com.bolsadeideas.springboot.inyecciondependencias.app.models.service;
 
+import org.springframework.stereotype.Component;
+
+//	Agregando MiServicio como un componente de Spring para poder realizar Inyeccion de dependencias
+@Component
+/*	Tambien con Service en lugar de Component
+@Service*/
 public class MiServicio {
 	
 	public String operacion() {
